39de20f7025554a23d21d55ad3911dce59cd7bed
levi <leviamullins@gmail.com>
2024-12-20 14:59:15 -0500

    added sapphire alongside raw_sapphire and a tab in creative to view these items

--- src/main/java/net/levi/tutorialmod/TutorialMod.java
+++ src/main/java/net/levi/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.levi.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,13 +27,17 @@ public class TutorialMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
-        // Register the commonSetup method for modloading
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+
+
         modEventBus.addListener(this::commonSetup);
 
-        // Register ourselves for server and other game events we are interested in
+
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
+
         modEventBus.addListener(this::addCreative);
 
     }
@@ -45,7 +50,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
